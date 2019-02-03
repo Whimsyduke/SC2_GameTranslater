a7c20bec911221c2e748cfddcb718360e38beeb6
Whimsy Elysium Duke <whimsyduke@163.com>
2019-02-03 12:12:22 +0800

    Add: Base language support and begining of Ribbon UI

--- SC2_GameTranslater/SC2_GameTranslater_Window.xaml.cs
+++ SC2_GameTranslater/SC2_GameTranslater_Window.xaml.cs
@@ -12,17 +12,120 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Fluent;
+using System.IO;
+using System.Globalization;
+
+using Globals = SC2_GameTranslater.Source.Class_Globals;
+using EnumLanguage = SC2_GameTranslater.Source.EnumLanguage;
 
 namespace SC2_GameTranslater
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class SC2_GameTranslater_Window : Window
+    public partial class SC2_GameTranslater_Window : RibbonWindow
     {
+        #region 字段属性
+
+        #region 属性
+
+        /// <summary>
+        /// 语言依赖项属性
+        /// </summary>
+        public static DependencyProperty EnumCurrentLanguageProperty = DependencyProperty.Register("EnumCurrentLanguage", typeof(EnumLanguage), typeof(SC2_GameTranslater_Window));
+
+        /// <summary>
+        /// 当前语言依赖项
+        /// </summary>
+        public EnumLanguage EnumCurrentLanguage
+        {
+            set
+            {
+                SetValue(EnumCurrentLanguageProperty, value);
+                ResourceDictionary_WindowLanguage.MergedDictionaries.Clear();
+                Globals.CurrentLanguage = Globals.DictUILanguages[value];
+                ResourceDictionary_WindowLanguage.MergedDictionaries.Add(Globals.CurrentLanguage);
+            }
+            get
+            {
+                return ((EnumLanguage)GetValue(EnumCurrentLanguageProperty));
+            }
+        }
+        #endregion
+
+        #region 字段
+        #endregion
+
+        #endregion
+
+        #region 构造函数
+        /// <summary>
+        /// 构造函数
+        /// </summary>
         public SC2_GameTranslater_Window()
         {
+            Globals.MainWindow = this;
             InitializeComponent();
+
+            #region 多语言配置
+            bool useDefault = true;
+            foreach (EnumLanguage select in Enum.GetValues(typeof(EnumLanguage)))
+            {
+                string languageName = Enum.GetName(typeof(EnumLanguage), select);
+                string fileName = "Language\\" + languageName + ".xaml";
+                FileInfo file = new FileInfo(fileName);
+                ResourceDictionary language = new ResourceDictionary();
+                if (file.Exists)
+                {
+                    language.Source = new Uri(file.FullName);
+                }
+                else
+                {
+                    switch (select)
+                    {
+                        case EnumLanguage.zhCN:
+                            language.Source = new Uri("pack://application:,,,/" + fileName);
+                            break;
+                        case EnumLanguage.enUS:
+                            language.Source = new Uri("pack://application:,,,/" + fileName);
+                            break;
+                        default:
+                            continue;
+                    }
+                }
+                string itemName = language["LanguageName"] as string;
+                Globals.DictComboBoxItemLanguage.Add(itemName, select);
+                Globals.DictUILanguages.Add(select, language);
+                ComboBox_Language.Items.Add(itemName);
+                if (CultureInfo.CurrentCulture.LCID == (int)select)
+                {
+                    ComboBox_Language.SelectedItem = itemName;
+                    useDefault = false;
+                }
+            }
+            if (useDefault)
+            {
+                ComboBox_Language.SelectedItem = Globals.DictUILanguages[EnumLanguage.enUS]["LanguageName"];
+            }
+            #endregion
         }
+        #endregion
+
+        #region 控件事件
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ComboBox_Language_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string itemName = ComboBox_Language.SelectedItem as string;
+            EnumCurrentLanguage = Globals.DictComboBoxItemLanguage[itemName];
+        }
+
+        #endregion
+
     }
 }
---
+++ SC2_GameTranslater/Source/Class_Globals.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using SC2_GameTranslater;
+
+namespace SC2_GameTranslater.Source
+{
+    #region 声明
+
+    /// <summary>
+    /// SC2本地化语言类型枚举
+    /// </summary>
+    public enum EnumLanguage
+    {
+        /// <summary>
+        /// 简体中文
+        /// </summary>
+        zhCN = 0x0804,
+        /// <summary>
+        /// 繁体中文
+        /// </summary>
+        zhTW = 0x0404,
+        /// <summary>
+        /// 英语美国
+        /// </summary>
+        enUS = 0x0409,
+        /// <summary>
+        /// 德语德国
+        /// </summary>
+        deDE = 0x0407,
+        /// <summary>
+        /// 西班牙语墨西哥
+        /// </summary>
+        esMX = 0x080A,
+        /// <summary>
+        /// 西班牙语西班牙
+        /// </summary>
+        esES = 0x0c0A,
+        /// <summary>
+        /// 法语法国
+        /// </summary>
+        frFR = 0x040C,
+        /// <summary>
+        /// 意大利语意大利
+        /// </summary>
+        itIT = 0x0410,
+        /// <summary>
+        /// 波兰语波兰
+        /// </summary>
+        plPL = 0x0415,
+        /// <summary>
+        /// 葡萄牙语巴西
+        /// </summary>
+        ptBR = 0x0416,
+        /// <summary>
+        /// 俄语俄罗斯
+        /// </summary>
+        ruRU = 0x0419,
+        /// <summary>
+        /// 韩语韩国
+        /// </summary>
+        koKR = 0x0412,
+    }
+
+    #endregion
+
+    /// <summary>
+    /// 全局数据
+    /// </summary>
+    static class Class_Globals
+    {
+
+        #region 属性字段
+
+        #region 属性
+
+        /// <summary>
+        /// 语言字典
+        /// </summary>
+        public static Dictionary<EnumLanguage, ResourceDictionary> DictUILanguages { set; get; }
+
+        /// <summary>
+        /// 列表项对应语言
+        /// </summary>
+        public static Dictionary<object, EnumLanguage> DictComboBoxItemLanguage { set; get; }
+
+        /// <summary>
+        /// 当前语言
+        /// </summary>
+        public static ResourceDictionary CurrentLanguage { set; get; }
+
+        /// <summary>
+        /// 主窗口
+        /// </summary>
+        public static SC2_GameTranslater_Window MainWindow { set; get; }
+
+        #endregion
+
+        #endregion
+
+        #region 构造函数
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        static Class_Globals()
+        {
+            DictUILanguages = new Dictionary<EnumLanguage, ResourceDictionary>();
+            DictComboBoxItemLanguage = new Dictionary<object, EnumLanguage>();
+        }
+
+        #endregion
+    }
+}
